b55b487731ecf6de5485aaf20d454cf9ec46ec90
Jim Deville <jdeville@microsoft.com>
2009-07-16 16:15:22 -0700

    python interop demo. Demo.rb is what to run

---
+++ demofiles/demo.rb
@@ -0,0 +1,15 @@
+require 'IronPython'
+dir = "C:\\Users\\jdeville\\projects\\irwitty\\demofiles"
+Dir.chdir(dir) do
+  e = IronPython::Hosting::Python.create_engine
+  $s = e.create_scope
+  e.execute 'from spelling import *', $s
+end
+class << e
+  def correct(str)
+    execute("correct('#{str}')", $s).to_s
+  end
+end
+
+e.correct "speling"
+
